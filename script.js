08e4df534808a6f0367d4d065251a8132b86a947
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-08-21 11:09:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.getElementById("level").addEventListener('click', e => {
+	let count = 1;
+	let current = e.target;
+
+	while(current.previousElementSibling) {
+		count++;
+		current = current.previousElementSibling;
+	}
+	
+	alert("The level of the element is: "+count);
+});
